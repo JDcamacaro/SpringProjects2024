2f435114be6e1132c6fa5ca1bb36504a1ff5a6fd
Jdcamacaro <davidjcamacaro@gmail.com>
2024-01-20 23:07:05 -0400

    cambio

--- Clientes/facturas/src/main/java/com/facturas/facturas/Repository/FactRepository.java
+++ Clientes/facturas/src/main/java/com/facturas/facturas/Repository/FactRepository.java
@@ -4,7 +4,14 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 
-public interface FactRepository {
+public interface FactRepository extends package packageName;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+//Remove @RepositoryRestResource below to disable auto REST api:
+@RepositoryRestResource
+public interface repositoryName extends CrudRepository<FactModel, ID>{} {
 
     
 }
